f7ecfa1fed4d6ba15b7966b3bf42228b49f1c080
Ruslan <ruslantsibrinskiy@mail.ru>
2024-11-03 07:02:46 +1000

    Login form and controller are ready

--- app/settings.py
+++ app/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 
 
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -153,3 +155,4 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
